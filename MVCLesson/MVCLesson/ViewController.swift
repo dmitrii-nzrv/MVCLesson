6ebab0df8154dad2d60d8d67a3bfa8e4e5a9f8e6
Dmitrii <dmitrii.nazarov7703@gmail.com>
2023-12-03 21:18:51 +0300

    project build

---
+++ MVCLesson/MVCLesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVCLesson
+//
+//  Created by Dmitrii Nazarov on 03.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
